f5b96a50b64a150aeab269ba0248e33f8928b516
Jashanjot Dhilon <jashanjotdhilon@gmail.com>
2021-08-18 21:54:11 -0600

    First commit

--- app.js
+++ app.js
@@ -35,11 +35,45 @@ app.use(passport.session());
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useCreateIndex', true);
 
+// --- Set up items and list -- //
+const itemSchema = new mongoose.Schema({
+    task: {
+        type: String,
+        required: true
+    }
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+    task: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+    task: "Hit + to add a new item."
+});
+
+const item3 = new Item({
+    task: "<-- Hit this to complete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = new mongoose.Schema({
+    name: String,
+    items: [itemSchema]
+});
+
+const List = mongoose.model("List", listSchema);
+
+
+// --- Set up for passport and OAuth --- //
+
 const userSchema = new mongoose.Schema({
+    list: listSchema,
     email: String,
     password: String,
-    googleId: String,
-    secret: String
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -62,7 +96,7 @@ passport.use(new passportLocal(User.authenticate()));
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "http://localhost:3000/auth/google/list",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"     // For G+ deprecation
     },
     function(accessToken, refreshToken, profile, cb) {
@@ -72,6 +106,7 @@ passport.use(new GoogleStrategy({
     });
 }));
 
+
 app.get('/', function(req, res){
     res.render('home');
 });
@@ -80,11 +115,11 @@ app.get('/auth/google',
     passport.authenticate('google', {scope: ['profile']})
 );
 
-app.get('/auth/google/secrets', 
+app.get('/auth/google/list', 
     passport.authenticate('google', { failureRedirect: '/login' }),
     function(req, res) {
         // Successful authentication, redirect
-        res.redirect('/secrets');
+        res.redirect('/list');
 });
 
 app.get('/login', function(req, res){
@@ -95,29 +130,42 @@ app.get('/register', function(req, res){
     res.render('register');
 });
 
-app.get('/secrets', function(req, res){
-    User.find({"secret": {$ne:null}}, function(err, results){
-        if(err) {
-           console.log(err);
-        } else {
-            if(results) {
-                res.render('secrets', {secretsList: results});
+app.get('/list', function(req, res){
+
+    // Find current user by ID, and render their list if found
+    if(req.isAuthenticated()) {
+        User.findById(req.user.id, function(err, result){
+            if(err) {
+                console.log(err);
+            } else {
+                // If list is empty, initialize it
+                if(!result.list) {
+                    const list = new List({
+                        name: "Your List",
+                        items: defaultItems
+                    });
+
+                    result.list = list;
+                    result.save();
+                    console.log(result);
+                    res.redirect("/list");
+                } 
+                // Show list if it exists
+                else {
+                    res.render("list", {listTitle: result.list.name, items: result.list.items});
+                }
             }
-        }
-    });
+        });
+    } 
+    else {
+        res.redirect('/login');
+    }
 });
 
+// TODO: Add logout button
 app.get('/logout', function(req, res){
     req.logout();
     res.redirect('/');
-});
-
-app.get('/submit', function(req, res){
-    if (req.isAuthenticated()){
-        res.render('submit');
-    } else {
-        res.redirect('/login');
-    }
 });
 
 app.post('/register', function(req, res){
@@ -127,31 +175,48 @@ app.post('/register', function(req, res){
            res.redirect('/register');
         } else {
             passport.authenticate('local')(req, res, function(){
-                res.redirect('/secrets');
+                res.redirect('/list');
             });
         }
     });
 });
 
 app.post('/login', passport.authenticate('local',  {failureRedirect: '/login'}), function(req, res){
-    res.redirect('/secrets');
+    res.redirect('/list');
 });
 
-app.post('/submit', function(req, res){
-    const secret = req.body.secret;
+// Add items by finding user and adding to their list items
+app.post("/add", function(req, res){
+    if(req.isAuthenticated()) {
+        const item = new Item({
+            task: req.body.newItem
+        });
 
-    User.findById(req.user.id, function(err, result){
-        if(err) {
-           console.log(err);
-        } else {
-            if(result) {
-                result.secret = secret;
-                result.save(function(){
-                    res.redirect('/secrets');
-                });
+        User.findByIdAndUpdate(req.user.id, {$addToSet: {'list.items' : item}}, {useFindAndModify: false}, function(err, result) {
+            if(err) {
+                console.log(err);
+            } else {
+                res.redirect("/list");
             }
-        }
-    });
+        });
+    }
+    else {
+        res.redirect('/login');
+    }
+    
+});
+
+// Delete items by first finding the user and their list, then pulling by ID
+app.post("/delete", function(req, res){
+    if(req.isAuthenticated()){
+    const itemID = req.body.checkbox;
+        User.findByIdAndUpdate(req.user.id, {$pull: {'list.items': {_id: itemID}}}, {useFindAndModify: false}, function(err, result){
+            if(err){
+                console.log(err);
+            }
+            res.redirect("/list");
+        });
+    }
 });
 
 app.listen(3000, function() {
